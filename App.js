7317f67b820d9427e4f1e2f3985940c7122a2d4b
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-15 10:51:03 -0300

    firebase auth exercise

--- App.js
+++ App.js
@@ -1,98 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, FlatList, ActivityIndicator} from 'react-native';
-import firebase from './src/firebaseConnection';
+import React from 'react';
+import Routes from './src/routes';
+import { LogBox } from 'react-native';
 
-console.disableYellowBox=true;
-
-export default function App(){
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [user, setUser] = useState('');
+LogBox.ignoreAllLogs(true);
 
 
-
-  async function logar(){
-    await firebase.auth().signInWithEmailAndPassword(email, password)
-    .then( (value) => {
-      alert('Bem-vindo: ' + value.user.email);
-      setUser(value.user.email);
-    })
-    .catch( (error) => {
-        alert('Ops algo deu errado!');
-        return;
-    })
-
-    setEmail('');
-    setPassword('');
-  }
-
-
-  async function logout(){
-    await firebase.auth().signOut();
-    setUser('');
-    alert('Deslgoado com sucesso!');
-  }
-
-  return(
-    <View style={styles.container}>
-      <Text style={styles.texto}>Email</Text>
-      <TextInput
-      style={styles.input}
-      underlineColorAndroid="transparent"
-      onChangeText={(texto) => setEmail(texto) }
-      value={email}
-      />
-
-      <Text style={styles.texto}>Senha</Text>
-      <TextInput
-      style={styles.input}
-      underlineColorAndroid="transparent"
-      onChangeText={(texto) => setPassword(texto) }
-      value={password}
-      />
-
-      <Button
-      title="Acessar"
-      onPress={logar}
-      />
-
-        <Text style={{marginTop: 20, marginBottom: 20, fontSize: 23, textAlign: 'center'}}>
-          {user}
-        </Text>
-
-      {user.length > 0 ? 
-      (
-        <Button
-        title="Deslogar"
-        onPress={logout}
-        />
-      ) : 
-      (
-        <Text style={{marginTop: 20, marginBottom: 20, fontSize: 23, textAlign: 'center'}}>
-          Nenhum usuario esta logado
-        </Text>
-      )}
-
-
-
-    </View>
+export default function App() {
+ return (
+   <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    margin: 10,
-  },
-  texto: {
-    fontSize: 20,
-  },
-  input:{
-    marginBottom: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#121212',
-    height: 45,
-    fontSize: 17
-  }
-});
