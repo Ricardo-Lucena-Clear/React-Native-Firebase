77af79b5c812c6da5fef7ab8530e0e5d44d907a3
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-15 12:22:56 -0300

    TasksApp - Creating tasks interface

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  SafeAreaView, 
+  TextInput, 
+  TouchableOpacity,
+  FlatList
+ } from 'react-native';
 
 import Login from './src/components/Login';
+import TaskList from './src/components/TaskList';
+
+let tasks = [
+  {key: '1', nome: 'Comprar Coca cola'},
+  {key: '2', nome: 'Estudar javascript' }
+]
 
 export default function App() {
   const [user, setUser] = useState(null);
+
+  const [newTask, setNewTask] = useState('')
 
 
  if(!user){
@@ -13,7 +29,28 @@ export default function App() {
 
  return (
    <SafeAreaView style={styles.container}>
-    <Text>DENTRO DA TELA TAREFAS</Text>
+    
+    <View style={styles.containerTask}>
+      <TextInput
+        style={styles.input}
+        placeholder="O que vai fazer hoje?"
+        value={newTask}
+        onChangeText={ (text) =>  setNewTask(text) }
+      />
+      <TouchableOpacity style={styles.buttonAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+
+
+    <FlatList
+      data={tasks}
+      keyExtractor={ item => item.key }
+      renderItem={ ({ item }) => (
+        <TaskList data={item} />
+      )}
+    />
+
    </SafeAreaView>
   );
 }
@@ -24,5 +61,31 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingHorizontal: 10,
     backgroundColor: '#F2f6fc'
+  },
+  containerTask:{
+    flexDirection: 'row'
+  },
+  input:{
+    flex:1, 
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#141414',
+    height: 45,
+  },
+  buttonAdd:{
+   backgroundColor: '#141414',
+   height: 45,
+   alignItems: 'center',
+   justifyContent: 'center',
+   marginLeft:  5,
+   paddingHorizontal: 14,
+   borderRadius: 4,
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 22,
   }
 })
