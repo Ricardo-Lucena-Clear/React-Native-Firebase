8f27fe19ba0f9963e80b4b7168a53936491edc67
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-15 12:18:59 -0300

    TasksApp - Registering and logging in

--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -4,13 +4,14 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDi8XANSDT9uoAT1OEtsANEALy0Xnzm0AY",
-    authDomain: "novo-app-73660.firebaseapp.com",
-    projectId: "novo-app-73660",
-    storageBucket: "novo-app-73660.appspot.com",
-    messagingSenderId: "806756658053",
-    appId: "1:806756658053:web:36a29793146e83a35032ec"
-  };
+  apiKey: "AIzaSyDi8XANSDT9uoAT1OEtsANEALy0Xnzm0AY",
+  authDomain: "novo-app-73660.firebaseapp.com",
+  databaseURL: "https://novo-app-73660-default-rtdb.firebaseio.com",
+  projectId: "novo-app-73660",
+  storageBucket: "novo-app-73660.appspot.com",
+  messagingSenderId: "806756658053",
+  appId: "1:806756658053:web:36a29793146e83a35032ec"
+};
   
 if (!firebase.apps.length){
     firebase.initializeApp(firebaseConfig)
--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,21 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from "firebase/app";
+import "firebase/database"
 import 'firebase/auth';
 
-let firebaseConfig = {
-  apiKey: "AIzaSyA9g1JdgAdfLf4mk6vJlq3qinYhU7YUSac",
-  authDomain: "meuapp-3354a.firebaseapp.com",
-  databaseURL: "https://meuapp-3354a.firebaseio.com",
-  projectId: "meuapp-3354a",
-  storageBucket: "meuapp-3354a.appspot.com",
-  messagingSenderId: "842683957873",
-  appId: "1:842683957873:web:1c1e69c05124d5de4b91d0",
-  measurementId: "G-GFJBH5JB9B"
-};
 
-if(!firebase.apps.length){
-  //Abrir minha conexao
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyDi8XANSDT9uoAT1OEtsANEALy0Xnzm0AY",
+  authDomain: "novo-app-73660.firebaseapp.com",
+  databaseURL: "https://novo-app-73660-default-rtdb.firebaseio.com",
+  projectId: "novo-app-73660",
+  storageBucket: "novo-app-73660.appspot.com",
+  messagingSenderId: "806756658053",
+  appId: "1:806756658053:web:36a29793146e83a35032ec"
+};
+  
+if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig)
+    
 }
 
 export default firebase;
